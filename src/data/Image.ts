84f075f5591589e5b410c2fa6a819d20fbc2bda4
lemasani <brianlemasan@gmail.com>
2025-06-22 11:58:19 +0300

    feat: enhance Home component with motion links and add image data module

---
+++ src/data/Image.ts
@@ -0,0 +1,9 @@
+
+
+export const ConstructionSiteImage = "https://images.unsplash.com/photo-1694521787673-28cbd8830ea5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8MA%3D%3D";
+
+export const ModernBuildingImage = "https://images.unsplash.com/photo-1515263487990-61b07816b324?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9kZXJuJTIwYnVpbGRpbmd8ZW58MHx8MHx8fDA%3D"
+
+export const CompletedBuildingImage = "https://images.unsplash.com/photo-1731254355255-13a30debca9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcGxldGVkJTIwYnVpbGRpbmclMjBhZnRlciUyMGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D"
+
+export const TeamAtWorkImage = "https://images.unsplash.com/photo-1646505256959-db71754a5790?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVhbSUyMGF0JTIwd29yayUyMCUyMGluJTIwY29uc3RydWN0aW9ufGVufDB8fDB8fHww"
