164367443d4788716c1b1fad3fb3aca346997573
lemasani <brianlemasan@gmail.com>
2025-08-26 18:50:51 +0300

    feat(projects): add multiple project entries with details and images
    --some projects do not have images

--- src/data/Image.ts
+++ src/data/Image.ts
@@ -6,3 +6,55 @@ export const CompletedBuildingImage = "https://res.cloudinary.com/deuwlmeyt/imag
 export const TeamAtWorkImage = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754906998/TBC_sabasaba_zfl4dt.jpg"
 
 
+// cfao images rennoavation
+
+export const CFAORenovationImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980034/IMG_2095_qwsbxv.jpg"
+export const CFAORenovationImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980027/IMG_2065_ghbaer.jpg"
+export const CFAORenovationImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980030/IMG_2070_ahl6au.jpg"
+export const CFAORenovationImage4 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980029/IMG_2076_qzbhl8.jpg"
+export const CFAORenovationImage5 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980029/IMG_2076_qzbhl8.jpg"
+export const CFAORenovationImage6 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980011/IMG_1126_vff8wu.jpg"
+
+// blue coast images 
+
+export const BlueCoastImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979926/WhatsApp_Image_2023-02-06_at_02.15.09_5_nmpr13.jpg"
+export const BlueCoastImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979932/WhatsApp_Image_2023-02-04_at_07.21.53_skldsn.jpg"
+export const BlueCoastImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979926/WhatsApp_Image_2023-02-06_at_02.15.09_5_nmpr13.jpg"
+export const BlueCoastImage4 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979934/WhatsApp_Image_2023-02-04_at_06.52.31_1_azel9r.jpg"
+
+// machine hiring 
+
+export const MachineHiring1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980179/IMG_2160_a9uexi.jpg"
+export const MachineHiring2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980180/IMG_2567_eat4a6.jpg"
+
+
+// house bunju 
+export const HouseBunjuImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980153/IMG_2327_uopdmd.jpg"
+export const HouseBunjuImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980156/IMG_2324_uff01m.jpg"
+
+
+// house wazo hill
+export const HouseWazoHillImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979783/IMG_2768_bqk1cg.jpg"
+export const HouseWazoHillImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979774/IMG_2767_aaoolk.jpg"
+export const HouseWazoHillImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754979783/IMG_2765_ooxg8d.jpg"
+
+
+// TBC world cup booth 
+export const TBCboothImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980211/IMG_0233_jjqlgf.jpg"
+export const TBCboothImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980209/IMG_0263_cmuj1y.jpg"
+export const TBCboothImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980211/IMG_0258_zbcyfn.jpg"
+export const TBCboothImage4 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980210/IMG_0264_zzyvxm.jpg"
+
+// TBDC fillin stations
+export const TBDCStationImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980241/4_qdy3ob.jpg"
+export const TBDCStationImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980243/2_kgrghq.jpg"
+export const TBDCStationImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980241/4_qdy3ob.jpg"
+export const TBDCStationImage4 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980237/1_sgliy5.jpg"
+
+
+// ZIC 
+export const ZICImage1 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980290/IMG_0948_gpbxql.jpg"
+export const ZICImage2 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980281/WhatsApp_Image_2022-11-21_at_1.17.08_PM_1_anzk5y.jpg"
+export const ZICImage3 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980287/IMG_0949_hyfbwc.jpg"
+export const ZICImage4 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980284/WhatsApp_Image_2022-11-21_at_1.17.08_PM_nczue4.jpg"
+export const ZICImage5 = "https://res.cloudinary.com/deuwlmeyt/image/upload/v1754980283/WhatsApp_Image_2022-11-21_at_1.17.09_PM_1_ujeykf.jpg"
